ca8538ea432984af1ccaca5746bc7591762174df
Petr Matrix <ptrmtrx@github>
2025-01-07 19:19:20 +0100

    fix compile with GCC UBSAN
    GCC errantly enables -fno-delete-null-pointer-checks.
    
    Ref: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=71962
    Ref: https://github.com/abseil/abseil-cpp/issues/1634#issuecomment-2576011026

--- absl/container/flat_hash_map.h
+++ absl/container/flat_hash_map.h
@@ -34,6 +34,7 @@
 
 #include <cstddef>
 #include <memory>
+#include <optional>
 #include <type_traits>
 #include <utility>
 
@@ -661,10 +662,10 @@ struct FlatHashMapPolicy {
   }
 
   template <class Hash>
-  static constexpr HashSlotFn get_hash_slot_fn() {
+  static constexpr std::optional<HashSlotFn> get_hash_slot_fn() {
     return memory_internal::IsLayoutCompatible<K, V>::value
-               ? &TypeErasedApplyToSlotFn<Hash, K>
-               : nullptr;
+               ? std::optional<HashSlotFn>(&TypeErasedApplyToSlotFn<Hash, K>)
+               : std::nullopt;
   }
 
   static size_t space_used(const slot_type*) { return 0; }
--- absl/container/flat_hash_set.h
+++ absl/container/flat_hash_set.h
@@ -34,6 +34,7 @@
 
 #include <cstddef>
 #include <memory>
+#include <optional>
 #include <type_traits>
 #include <utility>
 
@@ -559,8 +560,8 @@ struct FlatHashSetPolicy {
   static size_t space_used(const T*) { return 0; }
 
   template <class Hash>
-  static constexpr HashSlotFn get_hash_slot_fn() {
-    return &TypeErasedApplyToSlotFn<Hash, T>;
+  static constexpr std::optional<HashSlotFn> get_hash_slot_fn() {
+    return std::optional<HashSlotFn>(&TypeErasedApplyToSlotFn<Hash, T>);
   }
 };
 }  // namespace container_internal
